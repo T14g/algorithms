de62049a07f4f44c2a99cda49c801fb6894a0bd9
Tiago <ottiagos@gmail.com>
2021-07-21 00:59:47 -0300

    Quicksort Done

---
+++ grokking_algorithms/ch4/quicksort.js
@@ -0,0 +1,19 @@
+/**
+ * Quicksort is much faster than selection sort
+ */
+
+function quickSort(array) {
+
+    //0 or 1 elements
+    if (array.length < 2) {
+        return array;
+    } else {
+        let pivot = array[0];
+        let greater = array.filter((el) => el > pivot);
+        let less = array.filter((el) => el < pivot);
+
+        return quickSort(less) + pivot + quickSort(greater);
+    }
+}
+
+console.log(quickSort([1, 10, 5, 2, 3]));
