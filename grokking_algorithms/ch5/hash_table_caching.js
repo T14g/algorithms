b58540be94af20bddf5c32073dab87906b1dcde8
Tiago <ottiagos@gmail.com>
2021-07-25 23:47:26 -0300

    Hash table caching

--- grokking_algorithms/ch5/hash_table_caching.js
+++ grokking_algorithms/ch5/hash_table_caching.js
@@ -1,3 +1,25 @@
 /**
  * Hash tables can be used as cache
+ * It also dont allow duplicate entries
  */
+
+let cache = [];
+
+function getPageFromServer(url) {
+    return 'The page name is ' + url;
+}
+
+function getPage(url) {
+    if (cache[url]) {
+        return cache[url];
+    } else {
+        data = getPageFromServer(url);
+        cache[url] = data;
+        return data;
+    }
+}
+
+console.log(getPage('Home'));
+console.log(getPage('Home'));
+console.log(getPage('Contact'));
+console.log(cache);
